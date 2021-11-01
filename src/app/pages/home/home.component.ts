b241ffefba41796e5e26d268f4c0ec79509ba74a
Ibrahim Mumcu <mumcuibrahim@gmail.com>
2021-11-01 02:16:39 +0100

    anasayfa düzenlendi

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AppService } from 'src/app/services/app.service';
 import { Film } from '../../interfaces/film.interface';
@@ -6,19 +7,30 @@ import { Film } from '../../interfaces/film.interface';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
   latestFilms$: Observable<Film[]>;
   popularFilms$: Observable<Film[]>;
   featuredFilm$: Observable<Film>;
 
-  constructor(
-    private appService: AppService,
-  ) {
-    this.latestFilms$ = this.appService.getLatest();
-    this.popularFilms$ = this.appService.getPopular();
-    this.featuredFilm$ = this.appService.getFeatured();
+  films$: Observable<Film[]>;
+  sortBy: string;
+  page: number;
+
+  constructor(private appService: AppService, private activatedRoute: ActivatedRoute) {
+    this.activatedRoute.paramMap.subscribe((params: ParamMap) => {
+      this.sortBy = params.get('sortBy');
+      this.page = params.get('page') ? Number(params.get('page')) : 1;
+
+      if (this.sortBy === null) {
+        this.sortBy = 'latest';
+      }
+
+      this.films$ = this.appService.getAll(this.page, this.sortBy);
+    });
+    //this.latestFilms$ = this.appService.getLatest();
+    //this.popularFilms$ = this.appService.getPopular();
+    //this.featuredFilm$ = this.appService.getFeatured();
   }
 }
