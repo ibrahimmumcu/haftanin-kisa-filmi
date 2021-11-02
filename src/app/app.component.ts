f26f355b1022e39727bf4edcb166da62ad294e7d
Ibrahim Mumcu <mumcuibrahim@gmail.com>
2021-11-03 00:05:18 +0100

    search code is removed, header update, movie page video wrapper update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  
-  constructor(
-    private router: Router,
-  ) {
-    this.router.events.subscribe((event: NavigationEnd) => {
-      window.scroll(0, 0);
-    });
-  }
+  isMoviePage = false;
 
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        window.scroll(0, 0);
+        if (this.router.url.indexOf('/film/') > -1) {
+          this.isMoviePage = true;
+        } else {
+          this.isMoviePage = false;
+        }
+      });
+  }
 }
