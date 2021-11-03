1f1d4e82523de1fdbf8a51f8d8e8a585dc401d47
Ibrahim Mumcu <mumcuibrahim@gmail.com>
2021-11-03 20:25:15 +0100

    name of the featured component has been changed to "film-component"

--- src/app/components/film-player/film-player.component.ts
+++ src/app/components/film-player/film-player.component.ts
@@ -3,12 +3,11 @@ import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 import { Film } from '../../interfaces/film.interface';
 
 @Component({
-  selector: 'featured',
-  templateUrl: './featured.component.html',
-  styleUrls: ['./featured.component.scss']
+  selector: 'film-player',
+  templateUrl: './film-player.component.html',
+  styleUrls: ['./film-player.component.scss'],
 })
-export class FeaturedComponent implements OnInit, OnChanges {
-
+export class FilmPlayerComponent implements OnInit, OnChanges {
   @Input() film: Film;
   @Input() isFeatured: boolean;
   @HostBinding('style.background-image') backgroundImage: SafeStyle;
@@ -16,17 +15,14 @@ export class FeaturedComponent implements OnInit, OnChanges {
   @ViewChild('videoContainer') videoContainer: ElementRef;
   @ViewChild('checkCurtain') checkCurtain: ElementRef;
 
-  constructor(private sanitizer: DomSanitizer) {
-    
-  }
+  constructor(private sanitizer: DomSanitizer) {}
 
   ngOnInit(): void {
     this.setBackgroundImage();
   }
 
-
   ngOnChanges(changes: SimpleChanges) {
-    if(changes?.film?.currentValue?.featuredImage) {
+    if (changes?.film?.currentValue?.featuredImage) {
       this.setBackgroundImage();
       this.addAutoPlayToIframe();
       this.isPlayingClass = '';
@@ -34,9 +30,7 @@ export class FeaturedComponent implements OnInit, OnChanges {
   }
 
   setBackgroundImage() {
-    this.backgroundImage = this.sanitizer.bypassSecurityTrustStyle(
-      'url('+this.film?.featuredImage+')'
-    );
+    this.backgroundImage = this.sanitizer.bypassSecurityTrustStyle('url(' + this.film?.featuredImage + ')');
   }
 
   startPlaying() {
@@ -50,16 +44,15 @@ export class FeaturedComponent implements OnInit, OnChanges {
 
   addAutoPlayToIframe() {
     const parser = new DOMParser();
-    var parsedIframe = parser.parseFromString(this.film.videoEmbed, "text/html");
-    let iframe = parsedIframe.getElementsByTagName("iframe");
+    var parsedIframe = parser.parseFromString(this.film.videoEmbed, 'text/html');
+    let iframe = parsedIframe.getElementsByTagName('iframe');
     let src = iframe[0].src;
-    const paramMergerChar = src.indexOf('?') > -1 ? '&' : '?'; 
+    const paramMergerChar = src.indexOf('?') > -1 ? '&' : '?';
     this.film.videoEmbed = this.film.videoEmbed.replace(src, src + paramMergerChar + 'autoplay=1');
 
     const allowAttr = iframe[0].allow;
-    if(allowAttr.length === 0) {
+    if (allowAttr.length === 0) {
       this.film.videoEmbed = this.film.videoEmbed.replace('></iframe>', ' allow="autoplay; fullscreen"></iframe>');
     }
   }
-
 }
--- src/app/pages/film/film.module.ts
+++ src/app/pages/film/film.module.ts
@@ -2,20 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FilmComponent } from './film.component';
 import { FilmRoutesModule } from './film.routes';
-import { FeaturedModule } from 'src/app/components/featured/featured.module';
+import { FilmPlayerModule } from 'src/app/components/film-player/film-player.module';
 import { FilmCardModule } from 'src/app/components/film-card/film-card.module';
 import { SkeletonModule } from 'src/app/components/skeleton/skeleton.module';
 
 @NgModule({
-  declarations: [
-    FilmComponent,
-  ],
-  imports: [
-    CommonModule,
-    FilmRoutesModule,
-    FeaturedModule,
-    FilmCardModule,
-    SkeletonModule,
-  ]
+  declarations: [FilmComponent],
+  imports: [CommonModule, FilmRoutesModule, FilmPlayerModule, FilmCardModule, SkeletonModule],
 })
-export class FilmModule { }
+export class FilmModule {}
