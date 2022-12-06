a7948ad181e1075824f75f1b55cf556d929bf089
Ibrahim Mumcu <mumcuibrahim@gmail.com>
2022-12-06 23:05:17 +0100

    use new NgOptimizedImage directive

--- src/app/components/film-card/film-card.module.ts
+++ src/app/components/film-card/film-card.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { FilmCardComponent } from './film-card.component';
 import { RouterModule } from '@angular/router';
+
 @NgModule({
   declarations: [FilmCardComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, NgOptimizedImage],
   exports: [FilmCardComponent],
 })
 export class FilmCardModule {}
